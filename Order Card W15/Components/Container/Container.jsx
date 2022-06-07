cd9e456e485e3a48ede0ef816709424b61dfae52
Carl Ulvesten <ulvesten@icloud.com>
2022-06-07 13:10:08 +0200

    Background Image + Main Container Implemented

---
+++ Order Card W15/Components/Container/Container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Container.css';
+
+export const Container = () => {
+    return (
+        <div className='Container__Main'>
+            <div className='Container__Image'></div>
+        </div>
+    );
+};
--- Order Card W15/src/App.jsx
+++ Order Card W15/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Container } from '../Components/Container/Container';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className='App'>
-            <p>Hello Vite + React!</p>
+            <Container />
         </div>
     );
 }
